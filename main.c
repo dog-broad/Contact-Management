a830dd32ffd67e291f934de16830deb14b2fd703
dog-broad <74148058+dog-broad@users.noreply.github.com>
2022-05-01 18:52:31 +0530

    Main program and UI

---
+++ main.c
@@ -0,0 +1,391 @@
+#include <gtk/gtk.h>
+
+
+typedef struct ContactInfo
+{
+    gchar *Name;
+    gchar *Sur_Name;
+    gchar *Address;
+    gchar *Contact_Type;
+    gchar *email;
+    gchar *Number;
+}contact;
+
+
+
+typedef struct gtk_win_data
+{
+    GtkTreeIter iter;
+    GtkTreeIter comboIter;
+    GtkWidget *combo;
+    GtkWidget *liststore;
+    GtkWidget *window;
+    GtkBuilder *build;
+    gint contact_num;
+} win_data;
+
+
+gboolean on_close (GtkWidget*, gpointer);
+void Delete_Contact(GtkWidget*, gpointer);
+void Edit_Contact(GtkWidget*, gpointer);
+void Show_Contact(GtkButton*, gpointer);
+void Add_Contact(GtkButton*, gpointer);
+void Print_Contact(GtkButton*, gpointer);
+
+
+
+int main(int argc,char *argv[]){
+
+    win_data userdata;
+    userdata.contact_num=0;
+    gtk_init(&argc,&argv);
+    
+    userdata.build=gtk_builder_new_from_file("UI.glade");
+    userdata.window=(GtkWidget*) gtk_builder_get_object(userdata.build,"window");
+    g_signal_connect(G_OBJECT(userdata.window),"delete_event",G_CALLBACK(on_close),&userdata);
+
+    
+    
+    userdata.liststore=(GtkWidget*) gtk_builder_get_object(userdata.build,"liststore1");
+    userdata.combo=(GtkWidget*) gtk_builder_get_object(userdata.build,"combo");
+    
+    GtkCellRenderer* rcell1 = gtk_cell_renderer_text_new ();
+    GtkCellRenderer* rcell2 = gtk_cell_renderer_text_new ();
+    GtkCellRenderer* rcell5 = gtk_cell_renderer_text_new ();
+    
+    
+    gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(userdata.combo),rcell1,FALSE);
+    gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(userdata.combo),rcell1,"text",0,NULL);
+
+    gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(userdata.combo),rcell2,FALSE);
+    gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(userdata.combo),rcell2,"text",1,NULL);
+
+    gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(userdata.combo),rcell5,FALSE);
+    gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(userdata.combo),rcell5,"text",5,NULL);
+    
+
+    g_signal_connect(gtk_builder_get_object(userdata.build,"Display"),"clicked",G_CALLBACK(Show_Contact),&userdata);
+    g_signal_connect(gtk_builder_get_object(userdata.build,"AddContact"),"clicked",G_CALLBACK(Add_Contact),&userdata);
+    g_signal_connect(gtk_builder_get_object(userdata.build,"EditBtn"),"clicked",G_CALLBACK(Edit_Contact),&userdata);
+    g_signal_connect(gtk_builder_get_object(userdata.build,"Delete"),"clicked",G_CALLBACK(Delete_Contact),&userdata);    
+    g_signal_connect(gtk_builder_get_object(userdata.build,"Print"),"clicked",G_CALLBACK(Print_Contact),&userdata);
+    gtk_widget_show_all(userdata.window);
+
+    gtk_main();
+    return 0;
+}
+
+
+
+gboolean on_close (GtkWidget* gwt, gpointer data) {
+    gtk_main_quit ();
+    return TRUE;
+}
+
+void Delete_Contact(GtkWidget* gwt, gpointer data) 
+{
+    
+    win_data *UIdata = data;
+
+    GtkWidget* ErrorMsg=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Log");
+    if(gtk_combo_box_get_active_iter(GTK_COMBO_BOX(UIdata->combo),&UIdata->comboIter)==0)
+    {
+        gtk_label_set_text(GTK_LABEL(ErrorMsg),"You didn't chose a contact to delete in the ComboBox.");
+        return;
+    }
+    
+    gtk_list_store_remove(GTK_LIST_STORE(UIdata->liststore),&UIdata->comboIter);
+    
+    UIdata->contact_num-=1;
+    
+    GtkWidget* Contact_number=(GtkWidget*) gtk_builder_get_object(UIdata->build,"contact_num");
+    gchar nbr_str[10]; 
+    g_snprintf(nbr_str,10,"%d",UIdata->contact_num);
+    gtk_label_set_text(GTK_LABEL(Contact_number),nbr_str);
+}
+
+
+
+void Add_Contact(GtkButton *btn, gpointer data) {
+    win_data *UIdata = data;
+    GtkWidget* diag_Add_Contact=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Add_Edit_Contact");
+
+
+    
+    
+    GtkWidget* FirstName=(GtkWidget*) gtk_builder_get_object(UIdata->build,"FirstName");
+    GtkWidget* SurName=(GtkWidget*) gtk_builder_get_object(UIdata->build,"SurName");
+    GtkWidget* Address=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Address");
+    GtkWidget* T_Contact=(GtkWidget*) gtk_builder_get_object(UIdata->build,"T_Contact");
+    GtkWidget* Mail=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Mail");
+    GtkWidget* Num=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Num");
+
+    GtkWidget* EditError=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Log");
+    gtk_label_set_text(GTK_LABEL(EditError),"");
+
+    contact new_contact;
+
+    gtk_widget_show_all(diag_Add_Contact);
+
+    gint res =gtk_dialog_run(GTK_DIALOG(diag_Add_Contact));
+
+    if(gtk_entry_get_text_length(GTK_ENTRY(FirstName)) != 0 && gtk_entry_get_text_length(GTK_ENTRY(SurName)) != 0 &&
+        gtk_entry_get_text_length(GTK_ENTRY(Num)) != 0)
+        {
+        switch (res)
+            {
+            case GTK_RESPONSE_ACCEPT:
+            
+                new_contact.Name = (gchar*) gtk_entry_get_text(GTK_ENTRY(FirstName));
+                new_contact.Sur_Name = (gchar*) gtk_entry_get_text(GTK_ENTRY(SurName));
+                new_contact.Address = (gchar*) gtk_entry_get_text(GTK_ENTRY(Address));
+                new_contact.Contact_Type = (gchar*) gtk_entry_get_text(GTK_ENTRY(T_Contact));
+                new_contact.email = (gchar*) gtk_entry_get_text(GTK_ENTRY(Mail));
+                new_contact.Number = (gchar*) gtk_entry_get_text(GTK_ENTRY(Num));
+
+
+            
+            gtk_list_store_append(GTK_LIST_STORE(UIdata->liststore),&UIdata->iter);
+            gtk_list_store_set(GTK_LIST_STORE(UIdata->liststore),&UIdata->iter,
+                                                                    0,new_contact.Name,
+                                                                    1,new_contact.Sur_Name,
+                                                                    2,new_contact.Address,
+                                                                    3,new_contact.Contact_Type,
+                                                                    4,new_contact.email,
+                                                                    5,new_contact.Number
+                                                                    ,-1);  
+
+            UIdata->contact_num+=1;
+            
+             GtkWidget* Contact_number=(GtkWidget*) gtk_builder_get_object(UIdata->build,"contact_num");
+                gchar nbr_str[10];
+                g_snprintf(nbr_str,10,"%d",UIdata->contact_num);
+                gtk_label_set_text(GTK_LABEL(Contact_number),nbr_str);
+                break;
+
+            case GTK_RESPONSE_CANCEL:
+            case GTK_RESPONSE_NONE:
+                break;
+            }
+        }
+    else{
+    gtk_label_set_text(GTK_LABEL(EditError),"Mandatory fields have not been filled,\nAdding terminated");
+
+    }
+    
+    gtk_entry_set_text(GTK_ENTRY(FirstName),"");
+    gtk_entry_set_text(GTK_ENTRY(SurName),"");
+    gtk_entry_set_text(GTK_ENTRY(Address),"");
+    gtk_entry_set_text(GTK_ENTRY(T_Contact),"");
+    gtk_entry_set_text(GTK_ENTRY(Mail),"");
+    gtk_entry_set_text(GTK_ENTRY(Num),"");
+    gtk_widget_hide(diag_Add_Contact);
+}
+    
+
+
+void Edit_Contact(GtkWidget* btn, gpointer data)
+{
+    win_data *UIdata = data;
+    contact edit_contact;
+    GtkWidget* diag_Edit_Contact=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Add_Edit_Contact"); 
+
+    
+    GtkWidget* FirstName=(GtkWidget*) gtk_builder_get_object(UIdata->build,"FirstName");
+    GtkWidget* SurName=(GtkWidget*) gtk_builder_get_object(UIdata->build,"SurName");
+    GtkWidget* Address=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Address");
+    GtkWidget* T_Contact=(GtkWidget*) gtk_builder_get_object(UIdata->build,"T_Contact");
+    GtkWidget* Mail=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Mail");
+    GtkWidget* Num=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Num");
+
+    
+    GtkWidget* EditError=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Log");
+
+    
+    gtk_label_set_text(GTK_LABEL(EditError),"");
+
+    
+    if(gtk_combo_box_get_active_iter(GTK_COMBO_BOX(UIdata->combo),&UIdata->comboIter)==0)
+    {
+        gtk_label_set_text(GTK_LABEL(EditError),"You didn't chose a contact to edit in the ComboBox.");
+        return;
+    }
+
+    
+    gtk_tree_model_get(GTK_TREE_MODEL(UIdata->liststore),&UIdata->comboIter,
+                                                            0,&edit_contact.Name,
+                                                            1,&edit_contact.Sur_Name,
+                                                            2,&edit_contact.Address,
+                                                            3,&edit_contact.Contact_Type,        
+                                                            4,&edit_contact.email,
+                                                            5,&edit_contact.Number
+                                                            ,-1);                             
+
+    
+    gtk_entry_set_text(GTK_ENTRY(FirstName),edit_contact.Name);
+    gtk_entry_set_text(GTK_ENTRY(SurName),edit_contact.Sur_Name);
+    gtk_entry_set_text(GTK_ENTRY(Address),edit_contact.Address);
+    gtk_entry_set_text(GTK_ENTRY(T_Contact),edit_contact.Contact_Type);
+    gtk_entry_set_text(GTK_ENTRY(Mail),edit_contact.email);
+    gtk_entry_set_text(GTK_ENTRY(Num),edit_contact.Number);
+
+
+    
+    gtk_widget_show_all(diag_Edit_Contact);
+
+    gint res =gtk_dialog_run(GTK_DIALOG(diag_Edit_Contact));
+
+
+    
+    if(gtk_entry_get_text_length(GTK_ENTRY(FirstName)) != 0 && gtk_entry_get_text_length(GTK_ENTRY(SurName)) != 0 &&
+        gtk_entry_get_text_length(GTK_ENTRY(Num)) != 0)
+        {
+        switch (res)
+            {
+            case GTK_RESPONSE_ACCEPT:
+            
+                edit_contact.Name = (gchar*) gtk_entry_get_text(GTK_ENTRY(FirstName));
+                edit_contact.Sur_Name = (gchar*) gtk_entry_get_text(GTK_ENTRY(SurName));
+                edit_contact.Address = (gchar*) gtk_entry_get_text(GTK_ENTRY(Address));
+                edit_contact.Contact_Type = (gchar*) gtk_entry_get_text(GTK_ENTRY(T_Contact));
+                edit_contact.email = (gchar*) gtk_entry_get_text(GTK_ENTRY(Mail));
+                edit_contact.Number = (gchar*) gtk_entry_get_text(GTK_ENTRY(Num));
+            gtk_list_store_set(GTK_LIST_STORE(UIdata->liststore),&UIdata->comboIter,
+                                                                    0,edit_contact.Name,
+                                                                    1,edit_contact.Sur_Name,
+                                                                    2,edit_contact.Address,
+                                                                    3,edit_contact.Contact_Type,
+                                                                    4,edit_contact.email,
+                                                                    5,edit_contact.Number
+                                                                    ,-1);  
+            case GTK_RESPONSE_CANCEL:
+            case GTK_RESPONSE_NONE:
+                break;
+            }
+        }   
+    else{
+    gtk_label_set_text(GTK_LABEL(EditError),"Mandatory fields have not been filled,\nEditing terminated");
+
+    }
+    
+    
+    gtk_entry_set_text(GTK_ENTRY(FirstName),"");
+    gtk_entry_set_text(GTK_ENTRY(SurName),"");
+    gtk_entry_set_text(GTK_ENTRY(Address),"");
+    gtk_entry_set_text(GTK_ENTRY(T_Contact),"");
+    gtk_entry_set_text(GTK_ENTRY(Mail),"");
+    gtk_entry_set_text(GTK_ENTRY(Num),"");
+    gtk_widget_hide(diag_Edit_Contact);
+}
+
+
+void Show_Contact(GtkButton* btn, gpointer data)
+{
+    win_data *UIdata = data;
+    contact disp_contact;
+
+    
+    GtkWidget* diag_display_conatct = (GtkWidget*) gtk_builder_get_object(UIdata->build,"display_contact");
+
+    GtkWidget* FirstName=(GtkWidget*) gtk_builder_get_object(UIdata->build,"FirstName_disp");
+    GtkWidget* SurName=(GtkWidget*) gtk_builder_get_object(UIdata->build,"SurName_disp");
+    GtkWidget* Address=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Address_disp");
+    GtkWidget* T_Contact=(GtkWidget*) gtk_builder_get_object(UIdata->build,"T_Contact_disp");
+    GtkWidget* Mail=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Mail_disp");
+    GtkWidget* Num=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Num_disp");
+
+    GtkWidget* EditLog=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Log");
+    gtk_label_set_text(GTK_LABEL(EditLog),"");
+    if(gtk_combo_box_get_active_iter(GTK_COMBO_BOX(UIdata->combo),&UIdata->comboIter)==0)
+    {
+        gtk_label_set_text(GTK_LABEL(EditLog),"No Contact Slected, Display Terminated");
+        return;
+    }
+
+     gtk_tree_model_get(GTK_TREE_MODEL(UIdata->liststore),&UIdata->comboIter,
+                                                            0,&disp_contact.Name,
+                                                            1,&disp_contact.Sur_Name,
+                                                            2,&disp_contact.Address,
+                                                            3,&disp_contact.Contact_Type,        
+                                                            4,&disp_contact.email,
+                                                            5,&disp_contact.Number
+                                                            ,-1);   
+
+    gtk_label_set_text(GTK_LABEL(FirstName),disp_contact.Name);
+    gtk_label_set_text(GTK_LABEL(SurName),disp_contact.Sur_Name);
+    gtk_label_set_text(GTK_LABEL(Address),disp_contact.Address);
+    gtk_label_set_text(GTK_LABEL(T_Contact),disp_contact.Contact_Type);
+    gtk_label_set_text(GTK_LABEL(Mail),disp_contact.email);
+    gtk_label_set_text(GTK_LABEL(Num),disp_contact.Number);
+
+    gtk_widget_show_all(diag_display_conatct);
+
+    gint res =gtk_dialog_run(GTK_DIALOG(diag_display_conatct));
+    
+    switch (res)
+    {
+    case GTK_RESPONSE_ACCEPT: 
+        break;
+    
+    case GTK_RESPONSE_NONE:
+        break;
+    }
+
+    gtk_widget_hide(diag_display_conatct);
+}
+
+
+void Print_Contact(GtkButton *btn, gpointer data) 
+{
+    int valid;
+    int i;
+    char *filename = "test.txt";
+    FILE *fp = fopen(filename, "w");
+    char s1[30],s2[30],s3[30],s4[30],s5[30],s6[30];
+
+    fprintf(fp,"   _____            _             _     __  __                                   \n / ____|          | |           | |   |  \\/  |                                  \n| |     ___  _ __ | |_ __ _  ___| |_  | \\  / | __ _ _ __   __ _  __ _  ___ _ __ \n| |    / _ \\| '_ \\| __/ _` |/ __| __| | |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '__|\n | |___| (_) | | | | || (_| | (__| |_  | |  | | (_| | | | | (_| | (_| |  __/ |   \n  \\_____\\___/|_| |_|\\__\\__,_|\\___|\\__| |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|    \n                                                                 __/ |           \n                                                                |___/           \n");
+
+    win_data *UIdata = data;
+
+    contact edit_contact;
+
+    GtkWidget* EditError=(GtkWidget*) gtk_builder_get_object(UIdata->build,"Log");
+
+    gtk_label_set_text(GTK_LABEL(EditError),"");
+
+    if(gtk_combo_box_get_active_iter(GTK_COMBO_BOX(UIdata->combo),&UIdata->comboIter)==0)
+    {
+        gtk_label_set_text(GTK_LABEL(EditError),"You didn't chose a contact to print in the ComboBox.");
+        return;
+    }
+
+    
+    i = 0; 
+    valid = gtk_tree_model_get_iter_first(GTK_TREE_MODEL(UIdata->liststore),&UIdata->iter);
+    while(valid)
+    {
+        gtk_tree_model_get(GTK_TREE_MODEL(UIdata->liststore),&UIdata->iter,
+                                                                0,&edit_contact.Name,
+                                                                1,&edit_contact.Sur_Name,
+                                                                2,&edit_contact.Address,
+                                                                3,&edit_contact.Contact_Type,        
+                                                                4,&edit_contact.email,
+                                                                5,&edit_contact.Number
+                                                                ,-1);      
+        
+        g_print(edit_contact.Name);
+
+        if (fp == NULL)
+        {
+            printf("Error opening the file %s", filename);
+            exit(1);
+        }
+        fprintf(fp,"%-15s|%-15s|%-15s|%-15s|%-15s|%-15s|\n", edit_contact.Name, edit_contact.Sur_Name, edit_contact.Address, edit_contact.Contact_Type, edit_contact.email, edit_contact.Number);
+
+        i++;
+        valid = gtk_tree_model_iter_next(GTK_TREE_MODEL(UIdata->liststore),&UIdata->iter);
+    }
+    fscanf(fp,"%-15s%*c%-15s%*c%-15s%*c%-15s%-15s%*c%-15s", &s1,&s2,&s3,&s4,&s5,&s6);
+    g_print("%-15s %-15s %-15s %-15s %-15s %-15s",s1,s2,s3,s4,s5,s6);
+    fclose(fp);
+}
